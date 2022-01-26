a30dd9316e1c24544f83c0e5f2fa93b252ca4395
Shakeb Shamsi <97080292+ShakebShamsi@users.noreply.github.com>
2022-01-26 15:06:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,16 +3,21 @@
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title:addTitle.value,
+    text:addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
-//   console.log(notesObj);
+  addTitle.value = "";
   showNotes();
 });
 
@@ -30,8 +35,8 @@ function showNotes() {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
                 </div>`;
@@ -47,10 +52,6 @@ function showNotes() {
 // Function to delete a note
 
 function deleteNote(index) {
-
-    
-//   console.log("I am deleting", index);
-
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
@@ -63,12 +64,11 @@ function deleteNote(index) {
   showNotes();
 }
 
-
+//Function to search a note
 let search = document.getElementById('searchTxt');
 search.addEventListener("input", function(){
 
     let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
@@ -78,14 +78,7 @@ search.addEventListener("input", function(){
         else{
             element.style.display = "none";
         }
-        // console.log(cardTxt);
+      
     })
 })
 
-/*
-Further Features:
-1. Add Title
-2. Mark a note as Important
-3. Separate notes by user
-4. Sync and host to web server 
-*/ 
